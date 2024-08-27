4fedb9323512ed691fd79a418b7be0de42387200
Tom Joy <thwjoy@gmail.com>
2024-08-27 15:35:03 +0100

    shorten bio

--- components/AboutTalentive.tsx
+++ components/AboutTalentive.tsx
@@ -8,11 +8,9 @@ export default function AboutTalentive() {
 
             <div className="flex items-center mb-6">
                 <p className="text-gray-700">
-                Think current staff training is stuck in the dark age? Us, too. That&apos;s why we&apos;re on a mission to revolutionize the way employees learn and develop their skills.
+                Talentive is revolutionizing employee training with AI-powered, interactive learning.
                 <br/>
-                In an ideal world, you would have a tutor constantly by your side, guiding you through the learning process of your day to day job. Bur in reality, that&apos;s not currently possible. However, not for much longer.
-                <br/>
-                We&apos;re increasing the effectiveness of deskless worker training using engaging and interactive AI. Our vision is a world where training days are a thing of the past and eLearning isn&apos;t a fixed set of slides and multiple choice questions. It&apos;s a world where training is performed constantly on the job and one where it&apos;s possible to simulate your work environment before you even set foot on the premises. 
+                We&apos;re enhancing deskless worker training with AI, aiming for a future where training is continuous, on-the-job, and simulates your work environment before you start.
                 </p>
             </div>
 
@@ -20,7 +18,7 @@ export default function AboutTalentive() {
                 <img className="w-24 h-24 rounded-full mr-6" src="/konst_img.jpg" alt="Konstantin"/>
                 <div>
                     <h2 className="text-xl font-semibold mb-2">Konstantin Klingler</h2>
-                    <p className="text-gray-700">Konstantin is a Schwarzman Scholar and was named to Forbes’ 30 Under 30 list. He has experience in venture capital and startups, having worked with Global Founders Capital, HodlCo, Fizz (YC S21), and Spenmo (YC S22). He has successfully scaled multiple apps, including a math learning app, which was distributed by the Austrian Ministry of Education.</p>
+                    <p className="text-gray-700">Konstantin, a Schwarzman Scholar and Forbes 30 Under 30 honoree, has venture capital and startup experience with Global Founders Capital, HodlCo, Fizz (YC S21), and Spenmo (YC S22). He successfully scaled multiple apps, including a math learning app distributed by the Austrian Ministry of Education.</p>
                 </div>
             </div>
 
